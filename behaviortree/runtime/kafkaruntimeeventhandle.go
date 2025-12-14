e9d9320695f1aeb44ae1b1a41683c044518ad4c0
“huwenhai” <huwenhai@protonmail.com>
2025-12-14 10:05:25 +0800

    improve

--- behaviortree/runtime/kafkaruntimeeventhandle.go
+++ behaviortree/runtime/kafkaruntimeeventhandle.go
@@ -279,7 +279,15 @@ func (p *KafkaRuntimeEventHandle) NewStack(behaviorTree iface.IBehaviorTree, dat
 
 func (p *KafkaRuntimeEventHandle) RemoveStack(behaviorTree iface.IBehaviorTree, data *iface.StackRuntimeData, nowtimestampInMilli int64) {
 	p.handle.RemoveStack(behaviorTree, data, nowtimestampInMilli)
-	p.sendMessage([]byte(strconv.FormatInt(int64(data.StackID), 10)), nowtimestampInMilli, "remove_stack", behaviorTree.ID(), behaviorTree.Name(), behaviorTree.Unit().Name(), behaviorTree.Unit().ID()) //删除栈
+	data2 := map[string]interface{}{
+		"stack_id": data.StackID,
+	}
+	jsonData, err := json.Marshal(data2)
+	if err != nil {
+		p.log.Errorf("序列化数据失败: %v", err)
+		return
+	}
+	p.sendMessage(jsonData, nowtimestampInMilli, "remove_stack", behaviorTree.ID(), behaviorTree.Name(), behaviorTree.Unit().Name(), behaviorTree.Unit().ID()) //删除栈
 }
 
 func (p *KafkaRuntimeEventHandle) PreOnStart(behaviorTree iface.IBehaviorTree, taskRuntimeData *iface.TaskRuntimeData, stackRuntimeData *iface.StackRuntimeData, task iface.ITask) {
